b830c16a18e67cb31cce4ecf3c342682fe90680e
weekband <gyqja15@gmail.com>
2022-05-19 17:28:35 +0900

    hi

---
+++ springBasic/src/main/java/com/example/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,25 @@
+package com.example.core.discount;
+
+import com.example.core.annotation.MainDiscountPolicy;
+import com.example.core.member.Grade;
+import com.example.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+
+        if (member.getGrade() == Grade.VIP){
+            return price * discountPercent /100;
+        } else {
+            return 0;
+        }
+    }
+}

81f5ffe9a74cec7ae87244d8e0b7fb751833139c
weekband <gyqja15@gmail.com>
2022-05-20 09:56:29 +0900

    test

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServletApplication {
 
+    // git test
+
     public static void main(String[] args) {
         SpringApplication.run(ServletApplication.class, args);
     }

1326eb231f60e5569f102e3c73ae3bb6cd27583e
weekband <gyqja15@gmail.com>
2023-04-30 14:57:46 +0900

    서블릿 예외처리

--- exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,6 +1,5 @@
 package hello.exception.servlet;
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,19 +10,16 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 @Controller
 public class ErrorPageController {
-
     @RequestMapping("/error-page/404")
-    public String errorPage404(HttpServletRequest request,HttpServletResponse response){
+    public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 404");
         return "error-page/404";
     }
 
     @RequestMapping("/error-page/500")
-    public String errorPage500(HttpServletRequest request,HttpServletResponse response){
+    public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 500");
         return "error-page/500";
     }
-
-
-
 }
+

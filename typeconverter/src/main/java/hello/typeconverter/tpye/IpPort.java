da04cef06ce194e6ffc8d34ee16ce0b206060ecc
HyoBeom Moon <gyqja15@gmail.com>
2023-05-09 20:22:29 +0900

    스프링 타입 컨버터

---
+++ typeconverter/src/main/java/hello/typeconverter/tpye/IpPort.java
@@ -0,0 +1,18 @@
+package hello.typeconverter.tpye;
+
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
+public class IpPort {
+    private String ip;
+    private int port;
+
+    public IpPort(String ip, int port){
+        this.ip = ip;
+        this.port = port;
+    }
+
+}

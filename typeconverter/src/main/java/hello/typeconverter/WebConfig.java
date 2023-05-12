77b187129bd04e6a0fe8084d99de143421054440
HyoBeom Moon <gyqja15@gmail.com>
2023-05-12 20:05:36 +0900

    스프링 타입 컨버터

--- typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -3,6 +3,7 @@ package hello.typeconverter;
 import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,10 +12,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
-
-        registry.addConverter(new StringToIpPortConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        //주석처리 우선순위
+        //registry.addConverter(new StringToIpPortConverter());
+        //registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new StringToIntegerConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
